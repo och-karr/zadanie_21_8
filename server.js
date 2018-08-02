75ca56e586944e1cd9c622f5d20754ddf7b4b66b
karolinneczka <okaa93@gmail.com>
2018-08-02 15:01:50 +0200

    Second commit - add database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb://<och-karr>:<qwerty123>@ds263640.mlab.com:63640/database-1', {
     useMongoClient: true
 });
 
